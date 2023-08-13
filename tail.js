b9054db66fc8e11ddc312be0fb6524827e1b340e
Haneef29 <123555185+Haneef29@users.noreply.github.com>
2023-08-13 17:59:10 +0100

    Create tail.js

---
+++ tail.js
@@ -0,0 +1,16 @@
+const nav_item = document.querySelector('.small_nav');
+const open_nav = document.querySelector('.open_nav');
+const clos_nav = document.querySelector('.close_nav');
+
+function show_nav(){
+    nav_item.style.height = '260px';
+    clos_nav.style.display = ' block'
+    open_nav.style.display = ' none'
+    
+}
+function close_nav(){
+    nav_item.style.height = '0';
+    open_nav.style.display = ' block'
+    clos_nav.style.display = ' none'
+   
+}
